269fa236d1a04db4dc968b17d628c36c481c97ff
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2024-08-21 13:49:48 +0000

    Solve bridging day1 assignment

---
+++ year1/bridging/2024-8-21/Assignment.java
@@ -0,0 +1,14 @@
+class Centimeters {
+    public static void main(String args[]) {
+        // 25mins
+        int cm = 31300;
+        int yd = cm / 9144;
+        int ft = cm % 9144 / 3048;
+        int in = cm % 9144 % 3048 / 254;
+
+        System.out.printf("Enter length in centimeters: %.0f", cm / 100.0);
+        System.out.printf("\n %d yards", yd);
+        System.out.printf(" %d feet", ft);
+        System.out.printf(" %d inches", in);
+    }
+}
