76bea0e5dd4c9b28dfc375b368bd38802cc3a364
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2024-08-21 13:51:14 +0000

    bridging(day1): add alternate solution to the assignment

--- year1/bridging/2024-8-21/Assignment.java
+++ year1/bridging/2024-8-21/Assignment.java
@@ -10,5 +10,16 @@ class Centimeters {
         System.out.printf("\n %d yards", yd);
         System.out.printf(" %d feet", ft);
         System.out.printf(" %d inches", in);
+
+        // Alternate version 19mins
+        // int cm = 31300;
+        // int yd = cm / 254 / 36;
+        // int ft = cm / 254 / 12 % 3;
+        // int in = cm / 243 / 12 / 3 % 254;
+
+        // System.out.printf("Enter length in centimeters: %.0f", cm / 100.0);
+        // System.out.printf("\n %d yards", yd);
+        // System.out.printf(" %d feet", ft);
+        // System.out.printf(" %d inches", in);
     }
 }
