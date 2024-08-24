232c6d6283e8530f6f5155a7ed4533384d9c8a6c
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2024-08-24 14:10:03 +0000

    chore: fix indentation

--- year1/bridging/2024-8-22/ManualDivisionMultiplication.java
+++ year1/bridging/2024-8-22/ManualDivisionMultiplication.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 
 class ManualDivisionMultiplication {
-  public static void main(String args[]) {
-    Scanner console = new Scanner(System.in);
-    int firstNumber, secondNumber, quotient, remainder, product;
+    public static void main(String args[]) {
+        Scanner console = new Scanner(System.in);
+        int firstNumber, secondNumber, quotient, remainder, product;
 
-    System.out.print("Enter first number: ");
-    firstNumber = console.nextInt();
-    remainder = firstNumber;
+        System.out.print("Enter first number: ");
+        firstNumber = console.nextInt();
+        remainder = firstNumber;
 
-    System.out.print("Enter second number: ");
-    secondNumber = console.nextInt();
+        System.out.print("Enter second number: ");
+        secondNumber = console.nextInt();
 
-    quotient = 0;
-    while (remainder >= secondNumber) {
-      remainder = remainder - secondNumber;
-      quotient = quotient + 1;
+        quotient = 0;
+        while (remainder >= secondNumber) {
+            remainder = remainder - secondNumber;
+            quotient = quotient + 1;
+        }
+
+        product = 0;
+        while (secondNumber > 0) {
+            product = product + firstNumber;
+            secondNumber = secondNumber - 1;
+        }
+
+        System.out.println("Quotient is " + quotient);
+        System.out.println("Remainder is " + remainder);
+        System.out.println("Product is " + product);
     }
-
-    product = 0;
-    while (secondNumber > 0) {
-      product = product + firstNumber;
-      secondNumber = secondNumber - 1;
-    }
-
-    System.out.println("Quotient is " + quotient);
-    System.out.println("Remainder is " + remainder);
-    System.out.println("Product is " + product);
-  }
 }
 
